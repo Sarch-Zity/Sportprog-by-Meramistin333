eb06bf4444f47a26017d235c23fc82d92b52bee5
Sarch-Zity <artem.zity@gmail.com>
2022-08-02 17:40:46 +0700

    rearranging space in folders 2

--- ONUPRA/main/urls.py
+++ ONUPRA/main/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('you/', views.account),
+    path('registration/', views.reg_page),
 ]
--- ONUPRA/main/views.py
+++ ONUPRA/main/views.py
@@ -3,3 +3,9 @@ from django.http import HttpResponse
 
 def index (request):
     return render(request, 'main/index.html')
+
+def account (request):
+    return render(request, 'main/user_page.html')
+
+def reg_page (request):
+    return render(request, 'main/registration_form.html')
