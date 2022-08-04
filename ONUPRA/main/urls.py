ccc49dcda57d1f3eeacb9a854082324812bc9d11
Sarch-Zity <artem.zity@gmail.com>
2022-08-04 19:33:44 +0700

    login ver2
    i hope its last vesion and last big prpblem in project

--- ONUPRA/main/urls.py
+++ ONUPRA/main/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('', views.index, name='home'),
     path('you/', views.account, name='account'),
     path('signup/', views.reg_page, name='signup'),
-    # path('login/', views.login, name='login'),
-    path('login/', auth_views.LoginView.as_view(template_name = 'main/login_form2.html'), name='login'),
+    path('login/', LoginView.as_view(template_name='main/login_form.html'), name='login'),
 ]
--- ONUPRA/main/views.py
+++ ONUPRA/main/views.py
@@ -7,30 +7,36 @@ def index (request):
     return render(request, 'main/home.html')
 
 def account (request):
+    print(CustomUser.username)
     return render(request, 'main/user_page.html')
 
 def reg_page (request):
-    error = [""]
+    error = ""
+    error_username = ""
+    error_email = ""
     if request.method == 'POST':
         form = CustomUserCreationFrom(request.POST)
         email = request.POST.get('email')
         nickname = request.POST.get('username')
         if CustomUser.objects.filter(username=nickname).exists():
-            error.append("Ник error")
+            error_username = "Данное имя уже занято"
         if CustomUser.objects.filter(email=email).exists():
-            error.append("Почта error")
+            error_email = "Данный адрес электронной почты уже занято"
+        if CustomUser.objects.filter(email=email).exists():
+            error_email = "Данный адрес электронной почты уже занято"
         if form.is_valid():
             form.save()
             return redirect('login')
-        else:
-            error.append("error")
+        elif error_email == "" and error_username == "":
+            error = "Неизвестная нам ошибка"
 
     form = CustomUserCreationFrom()
     content = {
         'form': form,
-        'error': error
+        'error': error,
+        'error_username': error_username,
+        'error_email': error_email
     }
-    print(error)
     return render(request, 'main/registration_form.html', content)
 
 # def login (request):
